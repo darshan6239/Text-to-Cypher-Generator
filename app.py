9c8fefd8684556a320e8bef97c68112e5b820724
Darshan Patil <137216903+darshan6239@users.noreply.github.com>
2026-07-15 19:00:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from langchain_ollama import ChatOllama
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
-# --------------------------------------------------
+
 # PAGE CONFIG
 
 st.set_page_config(
